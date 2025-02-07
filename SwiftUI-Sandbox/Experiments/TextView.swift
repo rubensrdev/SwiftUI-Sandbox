f37543f8e9a50c5589e83ca80e60c8522e387ae4
Rubén <ruben.sr.apple@gmail.com>
2025-02-07 12:11:41 +0100

    Working with Text

--- SwiftUI-Sandbox/Experiments/TextView.swift
+++ SwiftUI-Sandbox/Experiments/TextView.swift
@@ -9,7 +9,50 @@ import SwiftUI
 
 struct TextView: View {
     var body: some View {
-        Text("Hello from the TextView!")
+		VStack {
+			// Basic text
+			Text("Hello from the TextView!")
+			Divider()
+			
+			// With font and weight
+			Text("Text with custom font and weight")
+				.font(.title)
+				.fontWeight(.bold)
+			Text("Other text with custom font and weight")
+				.font(.system(size: 26, weight: .thin, design: .rounded))
+			Divider()
+			
+			// With color
+			Text("Text with custom color")
+				.foregroundStyle(.blue)
+			Divider()
+			
+			// With gradient
+			Text("Text with gradient")
+				.foregroundStyle(.linearGradient(colors: [.cyan, .purple, .pink], startPoint: .leading, endPoint: .trailing))
+			Divider()
+			
+			// With weight and style
+			Text("Text with custom weight and style")
+				.fontWeight(.heavy)
+				.italic()
+				.underline()
+			Divider()
+			
+			// With multiline
+			Text("There is a text with much more content than the others, so it will be displayed in multiple lines. This is a test to check how the text behaves when it is too long.")
+				.multilineTextAlignment(.center)
+				.lineLimit(2)
+				.truncationMode(.tail)
+			Divider()
+				
+			// With spacing and kerning
+			Text("Text with custom spacing and kerning")
+				.kerning(5)
+				.lineSpacing(15)
+			
+		}
+		.padding()
     }
 }
 
