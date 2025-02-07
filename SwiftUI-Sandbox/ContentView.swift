d845cdaa4665393eebd4a7a80e452f798f3ba62a
Rubén <ruben.sr.apple@gmail.com>
2025-02-07 11:28:22 +0100

    Initial Commit

---
+++ SwiftUI-Sandbox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Sandbox
+//
+//  Created by Rubén Segura Romo on 7/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Sandbox/SwiftUI_SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SandboxApp.swift
+//  SwiftUI-Sandbox
+//
+//  Created by Rubén Segura Romo on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
