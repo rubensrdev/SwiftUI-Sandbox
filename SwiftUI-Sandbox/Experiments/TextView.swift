06a260ad9e9d743e21f5689bbd674fe561fdc4a4
Rubén <ruben.sr.apple@gmail.com>
2025-02-07 11:57:02 +0100

    Start with Text

---
+++ SwiftUI-Sandbox/Experiments/TextView.swift
@@ -0,0 +1,18 @@
+//
+//  TextView.swift
+//  SwiftUI-Sandbox
+//
+//  Created by Rubén Segura Romo on 7/2/25.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    var body: some View {
+        Text("Hello from the TextView!")
+    }
+}
+
+#Preview {
+    TextView()
+}
