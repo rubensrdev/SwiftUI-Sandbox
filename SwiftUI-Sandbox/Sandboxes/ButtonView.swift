968dcb25044cd1402b9c7bc3a4dcca71910a7e1c
Rubén <ruben.sr.apple@gmail.com>
2025-02-13 11:48:20 +0100

    add content to button view

--- SwiftUI-Sandbox/Sandboxes/ButtonView.swift
+++ SwiftUI-Sandbox/Sandboxes/ButtonView.swift
@@ -8,121 +8,170 @@
 import SwiftUI
 
 struct ButtonView: View {
-    var body: some View {
-		VStack {
-			
-			// Basic button
-			Button("Basic button") {
-				print("Button tapped!")
-			}
-			Divider()
-			
-			// With system styles
-			HStack {
-				Button("Default") {
-					print("Default button tapped!")
-				}
+	@State private var isTapped = false
+	var body: some View {
+		ScrollView {
+			VStack {
 				
-				Button("Bordered") {
-					print("Bordered button tapped!")
+				// Basic button
+				Button("Basic button") {
+					print("Button tapped!")
 				}
-				.buttonStyle(.bordered)
+				Divider()
 				
-				Button("Prominent") {
-					print("Prominent button tapped!")
+				// With system styles
+				HStack {
+					Button("Default") {
+						print("Default button tapped!")
+					}
+					
+					Button("Bordered") {
+						print("Bordered button tapped!")
+					}
+					.buttonStyle(.bordered)
+					
+					Button("Prominent") {
+						print("Prominent button tapped!")
+					}
+					.buttonStyle(.borderedProminent)
+					
+					Button("Plain") {
+						print("Plain button tapped!")
+					}
+					.buttonStyle(.plain)
 				}
-				.buttonStyle(.borderedProminent)
+				Divider()
 				
-				Button("Plain") {
-					print("Plain button tapped!")
+				// With custom styles
+				HStack {
+					Button("Custom button") {
+						print("Custom button tapped!")
+					}
+					.padding()
+					.background(.indigo)
+					.foregroundStyle(.white)
+					.clipShape(.capsule)
+					
+					Button("Custom button") {
+						print("Custom button tapped!")
+					}
+					.padding()
+					.background(.yellow)
+					.foregroundStyle(.pink)
+					.fontWeight(.light)
+					.clipShape(RoundedRectangle(cornerRadius: 10))
 				}
-				.buttonStyle(.plain)
-			}
-			Divider()
-			
-			// With custom styles
-			HStack {
-				Button("Custom button") {
-					print("Custom button tapped!")
-				}
-				.padding()
-				.background(.indigo)
-				.foregroundStyle(.white)
-				.clipShape(.capsule)
+				Divider()
 				
-				Button("Custom button") {
-					print("Custom button tapped!")
+				// With image
+				Button("With image", image: .pattern) {
+					print("button with image tapped!")
 				}
-				.padding()
-				.background(.yellow)
-				.foregroundStyle(.pink)
-				.fontWeight(.light)
-				.clipShape(RoundedRectangle(cornerRadius: 10))
-			}
-			Divider()
-			
-			// With image
-			Button("With image", image: .pattern) {
-				print("button with image tapped!")
-			}
-			Divider()
-			
-			// With SF Symbol
-			Button("With SF Symbol", systemImage: "paperplane") {
-				print("button with SF Symbol tapped!")
-			}
-			Divider()
-			
-			// With icon and text
-			Button {
-				print("button with icon and text tapped!")
-			} label: {
-				Label("With icon and text", systemImage: "arrow.forward.square")
-			}
-			Divider()
-			
-			// Only with image
-			Button {
-				print("Only with image tapped!")
-			} label: {
-				Image(.pattern)
-					.resizable()
-					.scaledToFit()
-					.frame(width: 100)
-					.clipShape(.buttonBorder)
-			}
-			Divider()
-			
-			// With image and text
-			Button {
-				print("Only with image tapped!")
-			} label: {
-				ZStack {
+				Divider()
+				
+				// With SF Symbol
+				Button("With SF Symbol", systemImage: "paperplane") {
+					print("button with SF Symbol tapped!")
+				}
+				Divider()
+				
+				// With icon and text
+				Button {
+					print("button with icon and text tapped!")
+				} label: {
+					Label("With icon and text", systemImage: "arrow.forward.square")
+				}
+				Divider()
+				
+				// Only with image
+				Button {
+					print("Only with image tapped!")
+				} label: {
 					Image(.pattern)
 						.resizable()
 						.scaledToFit()
-						.frame(width: 120)
+						.frame(width: 100)
 						.clipShape(.buttonBorder)
-					Text("Overlay text")
-						.foregroundStyle(.black)
-						.bold()
 				}
-			}
-			
-			// Disabled button
-			Button {
+				Divider()
 				
-			} label: {
-				Label("Now disabled", systemImage: "exclamationmark.triangle")
+				// With image and text
+				Button {
+					print("Only with image tapped!")
+				} label: {
+					ZStack {
+						Image(.pattern)
+							.resizable()
+							.scaledToFit()
+							.frame(width: 120)
+							.clipShape(.buttonBorder)
+						Text("Overlay text")
+							.foregroundStyle(.black)
+							.bold()
+					}
+				}
+				Divider()
+				
+				// Disabled button
+				Button {
+					
+				} label: {
+					Label("Now disabled", systemImage: "exclamationmark.triangle")
+				}
+				.buttonStyle(.bordered)
+				.disabled(true)
+				Divider()
+				
+				// Scale effect
+				Button {
+					isTapped.toggle()
+					print("Button with scale effect tapped!")
+				} label: {
+					Label("Button with scale effect", systemImage: "scale.3d")
+				}
+				.buttonStyle(.borderedProminent)
+				.scaleEffect(isTapped ? 1.2 : 1)
+				.animation(.spring, value: isTapped)
+				Divider()
+				
+				// Floating button
+				ZStack {
+					Color.gray.opacity(0.1).ignoresSafeArea()
+					VStack {
+						Spacer()
+						HStack {
+							Spacer()
+							Button {
+								print("Floating button tapped!")
+							} label: {
+								Image(systemName: "plus.circle.fill")
+									.resizable()
+									.scaledToFit()
+									.foregroundStyle(.green)
+									.frame(height: 50)
+							}
+							.padding()
+						}
+					}
+				}
+				Divider()
+				
+				// With navigation stack
+				NavigationStack {
+					VStack {
+						NavigationLink("To detail view") {
+							Text("Detail view")
+						}
+						.buttonStyle(.borderedProminent)
+						.padding()
+					}
+				}
+				
 			}
-			.buttonStyle(.bordered)
-			.disabled(true)
-
-			
 		}
-    }
+	}
 }
 
 #Preview {
-    ButtonView()
+	ButtonView()
 }
