f81ef2ecad26b3391290553efa38a5606439935d
Rubén <ruben.sr.apple@gmail.com>
2025-02-13 12:26:02 +0100

    Add labeled content view

---
+++ SwiftUI-Sandbox/Sandboxes/LabeledContent.swift
@@ -0,0 +1,18 @@
+//
+//  LabeledContent.swift
+//  SwiftUI-Sandbox
+//
+//  Created by Rubén Segura Romo on 13/2/25.
+//
+
+import SwiftUI
+
+struct LabeledContent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LabeledContent()
+}
