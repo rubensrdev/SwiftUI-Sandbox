f61b692a5fe99686c71ab9f1f4b64b314853b1a2
Rubén <ruben.sr.apple@gmail.com>
2025-02-17 10:54:16 +0100

    Add form view

---
+++ SwiftUI-Sandbox/Sandboxes/FormViewAndDataEntry.swift
@@ -0,0 +1,54 @@
+//
+//  FormView.swift
+//  SwiftUI-Sandbox
+//
+//  Created by Rubén Segura Romo on 17/2/25.
+//
+
+import SwiftUI
+
+struct FormViewAndDataEntry: View {
+	@State private var name: String = ""
+	@State private var notificationsEnabled: Bool = true
+	@State private var carModel: CarModel = .Yaris
+	enum CarModel: String, CaseIterable {
+		case Civic
+		case Chr
+		case Yaris
+		case Cx5
+	}
+	
+    var body: some View {
+		NavigationStack {
+			// Basic form structure
+			Form {
+				Section {
+					TextField("Your name:", text: $name)
+					Picker("Car model", selection: $carModel) {
+						ForEach(CarModel.allCases, id: \.self) { model in
+							Text(model.rawValue)
+						}
+					}
+				} header: {
+					Text("Personal Information")
+				} footer: {
+					Text("We will never share your personal data.")
+				}
+				
+				Section {
+					Toggle("Notifications", isOn: $notificationsEnabled)
+				} header: {
+					Text("Preferences")
+				}
+
+			}
+			.navigationTitle("Demo form")
+			// Style in ipad o mac
+			.formStyle(.grouped)
+		}
+    }
+}
+
+#Preview {
+    FormViewAndDataEntry()
+}
