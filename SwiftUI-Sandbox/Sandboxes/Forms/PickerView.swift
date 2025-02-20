d83991b25ef247a34312d002af794fa1f2c27f22
Rubén <ruben.sr.apple@gmail.com>
2025-02-20 10:22:56 +0100

    Organizando ficheros

---
+++ SwiftUI-Sandbox/Sandboxes/Forms/PickerView.swift
@@ -0,0 +1,18 @@
+//
+//  PickerView.swift
+//  SwiftUI-Sandbox
+//
+//  Created by Rubén Segura Romo on 20/2/25.
+//
+
+import SwiftUI
+
+struct PickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PickerView()
+}
